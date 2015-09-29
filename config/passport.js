9b53d7792c0edc1ebecb3d612226ebb33f927ba4
omniaura <omniaura5@gmail.com>
2015-09-29 16:27:49 -0700

    added profile image to user model

--- config/passport.js
+++ config/passport.js
@@ -45,6 +45,7 @@ module.exports = function(passport) {
           newUser.google.token = token;
           newUser.google.name = profile.displayName;
           newUser.google.email = profile.emails[0].value; // pull the first email
+          newUser.google.image = profile.photos[0].value;
           // save the user
           newUser.save(function(err) {
             if (err) throw err;
